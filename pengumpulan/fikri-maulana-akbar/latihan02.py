968b4dde79b8e7f1c9af070b08eb5abbe21c8618
Fikri Maulana <fikrmlana@gmail.com>
2026-05-18 09:45:54 +0700

    variabel

---
+++ pengumpulan/fikri-maulana-akbar/latihan02.py
@@ -0,0 +1,30 @@
+username = "Fikri"
+email = "fikri@gmail.com"
+password = "fikri123"
+nama = "Fikri"
+umur = 17
+hobi = "Main game"
+produk = "Shampoo"
+jumlah = 17
+harga = 2500
+
+print(username)
+print(email)
+print(password,"\n")
+
+print("Selamat datang: " , username)
+print("Email anda adalah: " , email)
+print("Dengan password: " , password,"\n")
+
+print("===== DATA DIRI =====")
+print("Nama : " , nama)
+print("Umur : " , umur)
+print("Hobi : " , hobi)
+print("====================\n")
+
+print("===== PESANAN =====")
+print("Produk : " , produk)
+print("Jumlah : " , jumlah)
+print("Harga : " , harga)
+print("TOTAL PESANAN : Rp. " , jumlah*harga )
+print("====================")
