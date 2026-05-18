f1e416563ad4a3801e5bf068c49ce61d2156915a
Fikri Maulana <fikrmlana@gmail.com>
2026-05-18 09:25:33 +0700

    print

---
+++ pengumpulan/fikri-maulana-akbar/latihan01.py
@@ -0,0 +1,12 @@
+print("Halo Dunia")
+
+print("Nama Saya: Fikri Maulana Akbar")
+print("Saya belajar python")
+
+print("Nama: Fikri Maulana Akbar")
+print("Alamat: Sidoarjo")
+print("Hobi: Main game")
+
+print("************************")
+print("*   HELLO PYTHON 🚀   *")
+print("************************")
