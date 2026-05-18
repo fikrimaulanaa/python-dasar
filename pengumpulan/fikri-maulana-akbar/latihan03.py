5b9d24122af0639fe526215152bd0f77de2f63d4
Fikri Maulana <fikrimlana03@gmail.com>
2026-05-18 19:43:01 +0700

    input/output

---
+++ pengumpulan/fikri-maulana-akbar/latihan03.py
@@ -0,0 +1,18 @@
+# nama = input("Masukkan nama: ")
+# umur = int(input("Masukkan umur: "))
+# alas = int(input("Masukkan nilai alas (cm): "))
+# tinggi = int(input("Masukkan nilai tinggi (cm): "))
+# derajat = int(100)
+
+# print("Halo ",nama,"\n")
+
+# print("Nama saya ",nama)
+# print("Umur saya ",umur," tahun","\n")
+
+# print("=== LUAS SEGITIGA ===")
+# print("Alas: ",alas," cm")
+# print("Tinggi: ",tinggi," cm")
+# print("Luas: ",alas*tinggi/2," cm2")
+# print("====================")
+
+# print(180-(360-((180-derajat)+120+90)))
