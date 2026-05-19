c01e462406ac1722b56dd3299f439ede91256f1a
Fikri Maulana <fikrimlana03@gmail.com>
2026-05-19 13:54:37 +0700

    operasi

---
+++ pengumpulan/fikri-maulana-akbar/latihan05.py
@@ -0,0 +1,70 @@
+# panjang = int(input("Masukkan panjang alas limas (cm): "))
+# tinggi = int(input("Masukkan tinggi limas (cm): "))
+
+# print("Luas alas limas adalah: ",panjang*panjang," cm2")
+# print("Volume limas adalah: ",panjang*panjang*tinggi/3," cm3")
+
+
+# suhu = int(input("Masukkan suhu: "))
+
+# if suhu >= 30:
+#     print("Status: Cuaca Panas")
+# elif suhu >= 20 and suhu <= 30:
+#     print("Status: Cuaca Normal")
+# elif suhu <= 20:
+#     print("Status: Cuaca Dingin")
+
+
+# tahun = int(input("Masukkan tahun: "))
+
+# if tahun % 4 == 0:
+#     print("Status: Tahun Kabisat")
+# else:
+#     print("Status: Bukan Tahun Kabisat")
+
+
+# jam = int(input("Masukkan jam: "))
+
+# if jam >= 5 and jam <= 11:
+#     print("Status: Pagi")
+# elif jam > 11 and jam < 15:
+#     print("Status: Siang")
+# elif jam >= 15 and jam <= 18:
+#     print("Status: Sore")
+# else:
+#     print("Status: Malam")
+
+
+# total_belanja = int(input("Masukkan total belanja: "))
+# print("Total belanja: Rp. ",total_belanja)
+
+# if total_belanja >= 100000:
+#     print("Diskon: 20%")
+#     print("Total bayar: Rp. ",total_belanja-(total_belanja*20/100))
+# elif total_belanja >= 50000 and total_belanja <= 100000:
+#     print("Diskon: 10%")
+#     print("Total bayar: Rp. ",total_belanja-(total_belanja*10/100))
+# else:
+#     print("Diskon: 0%")
+#     print("Total bayar: Rp. ",total_belanja)
+
+
+# jenis_kendaraan = input("Masukkan jenis kendaraan: ")
+# lama_parkir = int(input("Masukkan Lama Parkir (Jam): "))
+
+# print("===== STRUK PARKIR =====")
+# print("Jenis kendaraan: ",jenis_kendaraan)
+# print("Lama parkir: ", lama_parkir)
+
+# if jenis_kendaraan == "motor":
+#     if lama_parkir >= 1 and lama_parkir <= 3:
+#         print("Total bayar: Rp.",2000*lama_parkir)
+#     if lama_parkir > 3:
+#         print("Total bayar: Rp.", 1500*lama_parkir)
+# elif jenis_kendaraan == "mobil":
+#     if lama_parkir >= 1 and lama_parkir <= 3:
+#         print("Total bayar: Rp.",5000*lama_parkir)
+#     if lama_parkir > 3:
+#         print("Total bayar: ",4000*lama_parkir)
+
+# print("=======================")
