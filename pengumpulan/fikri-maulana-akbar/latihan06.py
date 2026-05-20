c23923321841f3787f73dccf7463dd6e24dad96b
Fikri Maulana <fikrimlana03@gmail.com>
2026-05-20 09:27:42 +0700

    latihan06

--- pengumpulan/fikri-maulana-akbar/latihan06.py
+++ pengumpulan/fikri-maulana-akbar/latihan06.py
@@ -36,3 +36,8 @@
 # for i in range(5,0,-1):
 #     print("*" * i)
 
+# for i in range(5,0,-1):
+#     print(" " * (i-1),"*" * (5-i+1))
+
+# for i in range(1,6,1):
+#     print(" " * (i-1),"*" * (5-((i-1))))
